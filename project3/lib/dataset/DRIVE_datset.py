c1cc8ca4be7d3f4fdf14bb2613c4fd14aaa977ff
KJMvanderHorst <115348690+KJMvanderHorst@users.noreply.github.com>
2025-10-31 21:12:50 +0100

    finall commit of tn

--- project3/lib/dataset/DRIVE_datset.py
+++ project3/lib/dataset/DRIVE_datset.py
@@ -1,28 +1,44 @@
 import torch
 import os
 import glob
-import PIL.Image as Image
+import numpy as np
+from PIL import Image
+
 DATA_PATH = '/dtu/datasets1/02516/'
 
-class DRIVE(torch.utils.data.Dataset):
-    def __init__(self, train, transform):
-        'Initialization'
+class CustomSegmentationDataset(torch.utils.data.Dataset):
+    def __init__(self, train=True, transform=None):
+        """
+        train: True for training set, False for test set
+        transform: transformations to apply to both images and masks
+        """
         self.transform = transform
-        data_path = os.path.join(DATA_PATH, 'train' if train else 'test')
-        self.image_paths = sorted(glob.glob(data_path + '/images/*.jpg'))
-        self.label_paths = sorted(glob.glob(data_path + '/labels/*.png'))
+
+        if train:
+            data_path = os.path.join(DATA_PATH, 'training')
+            self.image_paths = sorted(glob.glob(os.path.join(data_path, 'images', '*.tif')))
+            # You can choose either 'mask' or '1st_manual' for masks
+            self.mask_paths = sorted(glob.glob(os.path.join(data_path, 'mask', '*.gif')))
+        else:
+            data_path = os.path.join(DATA_PATH, 'test')
+            self.image_paths = sorted(glob.glob(os.path.join(data_path, 'images', '*.tif')))
+            self.mask_paths = sorted(glob.glob(os.path.join(data_path, 'mask', '*.gif')))
+
+        assert len(self.image_paths) == len(self.mask_paths), "Number of images and masks should match!"
 
     def __len__(self):
-        'Returns the total number of samples'
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        'Generates one sample of data'
-        image_path = self.image_paths[idx]
-        label_path = self.label_paths[idx]
+        # Load image and mask
+        image = Image.open(self.image_paths[idx])
+        mask = Image.open(self.mask_paths[idx])
 
-        image = Image.open(image_path)
-        label = Image.open(label_path)
-        Y = self.transform(label)
-        X = self.transform(image)
+        # Apply transform if provided
+        if self.transform:
+            X = self.transform(image)
+            Y = self.transform(mask)
+        else:
+            X = torch.tensor(np.array(image), dtype=torch.float32)
+            Y = torch.tensor(np.array(mask), dtype=torch.float32)
         return X, Y

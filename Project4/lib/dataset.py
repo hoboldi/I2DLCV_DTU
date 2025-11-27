772929c173060ec7094b65ff9827fafed5e23bad
WeinanXiong <125128488+WeinanXiong@users.noreply.github.com>
2025-11-27 16:27:39 +0100

    return boxes location

--- Project4/lib/dataset.py
+++ Project4/lib/dataset.py
@@ -1,7 +1,4 @@
-
-
-# Helper functions
-
+#Helper functions
 def parse_voc_xml(xml_path):
     tree = ET.parse(xml_path)
     root = tree.getroot()
@@ -14,10 +11,7 @@ def parse_voc_xml(xml_path):
         ymin = int(bnd.find("ymin").text)
         xmax = int(bnd.find("xmax").text)
         ymax = int(bnd.find("ymax").text)
-        objects.append({
-            "class": cls,
-            "bbox": [xmin, ymin, xmax, ymax]
-        })
+        objects.append({"class": cls, "bbox": [xmin, ymin, xmax, ymax]})
 
     return objects
 
@@ -38,10 +32,13 @@ def iou(boxA, boxB):
     union = boxAArea + boxBArea - interArea
     return interArea / union if union > 0 else 0
 
-
+# ============================================================
+# 2. Dataset
+# ============================================================
 class PotholeDataset(Dataset):
     def __init__(self, root, proposal_dir, img_ids,
                  proposals_per_image=64,
+                 pos_ratio=0.25,
                  pos_thresh=0.7,
                  neg_thresh=0.5):
 
@@ -52,20 +49,19 @@ class PotholeDataset(Dataset):
         self.ids = img_ids
 
         self.proposals_per_image = proposals_per_image
+        self.num_pos = int(proposals_per_image * pos_ratio)
+        self.num_neg = proposals_per_image - self.num_pos
         self.pos_thresh = pos_thresh
         self.neg_thresh = neg_thresh
 
-        self.transform = transforms.Compose([
+        self.base_transform = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ])
 
-        # Build class mapping (background = 0)
         classes = set()
         for img_id in img_ids:
             xml = os.path.join(self.annotations, img_id + ".xml")
@@ -80,78 +76,73 @@ class PotholeDataset(Dataset):
     def to_xyxy(self, p):
         if len(p) == 4:
             x, y, w, h = p
-        elif len(p) == 5:
-            x, y, w, h = p[:4]
-        elif len(p) == 6:
+        elif len(p) == 5:  
             x, y, w, h = p[:4]
         else:
             raise ValueError("Unknown proposal format:", p)
-
         return [int(x), int(y), int(x+w), int(y+h)]
 
     def __getitem__(self, idx):
         img_id = self.ids[idx]
-
-        # Load image
         img_path = os.path.join(self.images, img_id + ".png")
-        img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
 
-        # Load ground truth
+        # Ground truth objects
         objects = parse_voc_xml(os.path.join(self.annotations, img_id + ".xml"))
         for obj in objects:
             obj["label_idx"] = self.class_to_idx[obj["class"]]
 
-        # Load proposals
-        raw_props = np.load(
-            os.path.join(self.proposals_dir, img_id + ".npy"),
-            allow_pickle=True
-        )
+        # load proposals
+        raw_props = np.load(os.path.join(self.proposals_dir, img_id + ".npy"), allow_pickle=True)
         proposals = [self.to_xyxy(p) for p in raw_props]
 
         # Label proposals
-        labeled = []
+        positives, negatives = [], []
+
         for p in proposals:
-            best_i, best_label = 0, 0
+            best_iou = 0
+            assigned_label = 0
             for gt in objects:
                 i = iou(p, gt["bbox"])
-                if i > best_i:
-                    best_i = i
-                    best_label = gt["label_idx"]
+                if i > best_iou:
+                    best_iou = i
+                    assigned_label = gt["label_idx"]
 
-            if best_i >= self.pos_thresh:
-                labeled.append((p, best_label))
-            elif best_i < self.neg_thresh:
-                labeled.append((p, 0))
+            if best_iou >= self.pos_thresh:
+                positives.append((p, assigned_label))
+            elif 0.1 < best_iou < self.neg_thresh:
+                negatives.append((p, 0))
 
-        #  Adressing the class imbalance issue (25% pos, 75% neg)
-        total = self.proposals_per_image
-        num_pos = int(0.25 * total)
-        num_neg = total - num_pos
 
-        positives = [(p, l) for (p, l) in labeled if l != 0]
-        negatives = [(p, 0) for (p, l) in labeled if l == 0]
+        # sample positives (up to num_pos)
+        if len(positives) > self.num_pos:
+            positives = random.sample(positives, self.num_pos)
 
-        random.shuffle(positives)
-        random.shuffle(negatives)
+        # sample negatives (up to num_neg)
+        if len(negatives) > self.num_neg:
+            negatives = random.sample(negatives, self.num_neg)
 
-        samples = positives[:num_pos] + negatives[:num_neg]
+        samples = positives + negatives
+        random.shuffle(samples)
 
-        crops, labels = [], []
+        crops, labels, boxes = [], [], []
 
         for box, label in samples:
             x1, y1, x2, y2 = box
             crop = img[y1:y2, x1:x2]
+
             if crop.size == 0:
                 continue
-            crop = self.transform(crop)
+
+            crop = self.base_transform(crop)
             crops.append(crop)
             labels.append(label)
+            boxes.append(box)
 
         if len(crops) == 0:
-            crops = [torch.zeros(3, 224, 224)]
-            labels = [0]
+            return (torch.zeros(1, 3, 224, 224),
+                    torch.tensor([0]),
+                    img_id,
+                    [[0,0,0,0]])
 
-        return torch.stack(crops), torch.tensor(labels), img_id, box
-
-
+        return torch.stack(crops), torch.tensor(labels), img_id, boxes
